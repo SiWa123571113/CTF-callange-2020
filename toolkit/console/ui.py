afc016d2237c77b7ad0c0dc9c968cdd7ebfacdf0
AlexanderHD27 <alexander.wersching@t-online.de>
2020-08-21 21:23:18 +0200

    Added getChar

--- toolkit/console/ui.py
+++ toolkit/console/ui.py
@@ -1,6 +1,30 @@
 from toolkit.console import color # pylint: disable=import-error
 import sys
 
+def _find_getch():
+    try:
+        import termios
+    except ImportError:
+        # Non-POSIX. Return msvcrt's (Windows') getch.
+        import msvcrt
+        return msvcrt.getch
+
+    # POSIX system. Create and return a getch that manipulates the tty.
+    import sys, tty
+    def _getch():
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(fd)
+            ch = sys.stdin.read(1).encode("utf-8")
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+    return _getch
+
+getchr = _find_getch()
+
 def rotStringRect(string: str):
     a = string.split("\n")
     b = []
